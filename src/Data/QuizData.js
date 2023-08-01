464b94e3cb97cc74577467f086f88fa6b1f0f557
Abdul Malik Javed <136786518+abdulmalikjaved@users.noreply.github.com>
2023-08-01 11:42:17 -0700

    Add files via upload

---
+++ src/Data/QuizData.js
@@ -0,0 +1,42 @@
+export const QuizData = [
+  {
+    question: "What does HTML stand for?",
+    options: [
+      "Hypertext Markup Language",
+      "Hypertext Markdown Language",
+      "Hyperloop Machine Language",
+      "Helicopters Terminals Motorboats Lamborginis"
+    ],
+    answer: 1
+  },
+  {
+    question: "Who is the father of HTML?",
+    options: ["Tim Berners-Lee", "Brendan Eich", "Sergey Brin"],
+    answer: 1
+  },
+  {
+    question: "What does CSS stand for?",
+    options: [
+      "Central Style Sheets",
+      "Cascading Style Sheets",
+      "Cascading Simple Sheets",
+      "Cars SUVs Sailboats"
+    ],
+    answer: 2
+  },
+  {
+    question: "How many types of levels in style sheets?",
+    options: ["One level", "Two level", "Three level", "Four level"],
+    answer: 3
+  },
+  {
+    question: "Javascript is a _____ language?",
+    options: ["Programming", "Application", "Scripting", "None of the above"],
+    answers: 1
+  },
+  {
+    question: "Which of the following is the correct name of React.js?",
+    options: ["React", "React.js", "ReactJS", "All of the above"],
+    answers: 4
+  }
+];
